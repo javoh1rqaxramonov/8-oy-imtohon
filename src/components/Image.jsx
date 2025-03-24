abd574fb47d21e338109dc354eca69546ef53fdb
javoh1rqaxramonov <ubotirov22@.com>
2025-03-24 16:11:11 +0500

    exam

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -4,73 +4,71 @@ import { useGlobalContext } from "../hooks/useGlobalContext";
 import { Link } from "react-router-dom";
 
 function Image({ image, added }) {
-  const { links, urls, alt_description, user, id } = image;
+  const { links, urls, alt_description, user } = image;
   const { LikedImages, dispatch } = useGlobalContext();
-
-  const addLikedImage = (event) => {
+  const addLikedImage = (image, event) => {
     event.preventDefault();
-    event.stopPropagation(); // Link navigatsiyasini to'xtatish
-    const alreadyAdded = LikedImages.some((img) => img.id === image.id);
+    // console.log(image);
+    const alreadyAdded = LikedImages.some((img) => {
+      return img.id === image.id;
+    });
 
     if (!alreadyAdded) {
       dispatch({ type: "LIKE", payload: image });
     } else {
       dispatch({ type: "DISLIKE", payload: image.id });
     }
+
+    // dispatch({ type: "LIKE", payload: image });
   };
 
-  const downloadImage = (event) => {
+  const downloadImage = (event)=>{
     event.preventDefault();
-    event.stopPropagation(); // Link navigatsiyasini to'xtatish
-    window.open(links.download + "&force=true", "_blank");
-  };
+    window.open(links.download + "&force=true",'_blank')
+  }
 
   return (
-    <div className="relative group">
-      <Link to={`/imageInfo/${image.id}`}>
+    <Link to='/imageInfo'>
+      <div className="relative group">
+        {!added && (
+          <span
+            onClick={(event) => addLikedImage(image, event)}
+            className="absolute h-8 w-8 rounded-xl border-1 hover-icon flex justify-center items-center  cursor-pointer right-2 top-2"
+          >
+            <FaRegHeart className="text-white" />
+          </span>
+        )}
+        {added && (
+          <span
+            onClick={(event) => addLikedImage(image ,event)}
+            className="absolute hover-icon h-8 w-8 rounded-xl border-1  flex justify-center items-center  cursor-pointer right-2 top-2 bg-white"
+          >
+            <FaHeart className="text-red-500" />
+          </span>
+        )}
         <img
           src={urls.regular}
-          alt={alt_description || `${user.name}'s photo`}
+          alt={alt_description}
           className="w-full rounded-md"
         />
-      </Link>
-
-      {!added && (
-        <span
-          onClick={addLikedImage}
-          className="absolute h-8 w-8 rounded-xl border border-gray-300 hover:bg-gray-100 flex justify-center items-center cursor-pointer right-2 top-2"
-        >
-          <FaRegHeart className="text-white" />
+        <span className="absolute left-2 bottom-2 flex items-center gap-2 hover-icon">
+          <img
+            src={user.profile_image.large}
+            alt={user.name + "avatar"}
+            className="w-5 h-5 md:w-8  md:h-8 rounded-full "
+          />
+          <p className="text-white text-sm hidden md:block">{user.name}</p>
+          <p className="text-white text-xs w-10 md:hidden">
+            {user.first_name.replace(/[^a-zA-Z\s]/g, "")}
+          </p>
         </span>
-      )}
-      {added && (
-        <span
-          onClick={addLikedImage}
-          className="absolute h-8 w-8 rounded-xl border border-gray-300 hover:bg-gray-100 flex justify-center items-center cursor-pointer right-2 top-2 bg-white"
-        >
-          <FaHeart className="text-red-500" />
+        <span className="absolute hover-icon h-8 w-8 flex justify-center items-center  cursor-pointer right-2 bottom-2 ">
+          <span onclik={(e)=>downloadImage(e)} >
+            <FaDownload className="text-white" />
+          </span>
         </span>
-      )}
-
-      <span className="absolute left-2 bottom-2 flex items-center gap-2 group-hover:opacity-100 opacity-0 transition-opacity">
-        <img
-          src={user.profile_image.large}
-          alt={`${user.name}'s avatar`}
-          className="w-5 h-5 md:w-8 md:h-8 rounded-full"
-        />
-        <p className="text-white text-sm hidden md:block">{user.name}</p>
-        <p className="text-white text-xs w-10 md:hidden">
-          {user.first_name.replace(/[^a-zA-Z\s]/g, "")}
-        </p>
-      </span>
-
-      <span
-        onClick={downloadImage}
-        className="absolute h-8 w-8 flex justify-center items-center cursor-pointer right-2 bottom-2 group-hover:opacity-100 opacity-0 transition-opacity"
-      >
-        <FaDownload className="text-white" />
-      </span>
-    </div>
+      </div>
+    </Link>
   );
 }
 
